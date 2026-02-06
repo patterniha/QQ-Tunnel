072b6a16968e82886c7426b84162697a32135f2c
patterniha <71074308+patterniha@users.noreply.github.com>
2026-02-06 19:11:45 +0330

    add compute_max

--- utility/base32.py
+++ utility/base32.py
@@ -1,16 +1,16 @@
 import base64
 
-BASE32_LIST = [b"A", b"B", b"C", b"D", b"E", b"F", b"G", b"H", b"I", b"J", b"K", b"L", b"M", b"N", b"O", b"P",
-               b"Q", b"R", b"S", b"T", b"U", b"V", b"W", b"X", b"Y", b"Z", b"2", b"3", b"4", b"5", b"6", b"7"]
+BASE32_LIST_UPPER = [b"A", b"B", b"C", b"D", b"E", b"F", b"G", b"H", b"I", b"J", b"K", b"L", b"M", b"N", b"O", b"P",
+                     b"Q", b"R", b"S", b"T", b"U", b"V", b"W", b"X", b"Y", b"Z", b"2", b"3", b"4", b"5", b"6", b"7"]
 
 BASE32_LIST_LOWER = [b"a", b"b", b"c", b"d", b"e", b"f", b"g", b"h", b"i", b"j", b"k", b"l", b"m", b"n", b"o", b"p",
                      b"q", b"r", b"s", b"t", b"u", b"v", b"w", b"x", b"y", b"z", b"2", b"3", b"4", b"5", b"6", b"7"]
 
-BASE32_CHARS_BYTES = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+BASE32_CHARS_BYTES_UPPER = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
 BASE32_CHARS_BYTES_LOWER = b"abcdefghijklmnopqrstuvwxyz234567"
 
 BASE32_LOOKUP = [-1] * 256
-for i, ch in enumerate(BASE32_CHARS_BYTES):
+for i, ch in enumerate(BASE32_CHARS_BYTES_UPPER):
     BASE32_LOOKUP[ch] = i
     lower_ch = BASE32_CHARS_BYTES_LOWER[i]
     if lower_ch != ch:
