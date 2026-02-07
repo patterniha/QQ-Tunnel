9ff3a4a5f6a3562c607540498c7db9c1805cd3a0
patterniha <71074308+patterniha@users.noreply.github.com>
2026-02-08 00:51:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -144,27 +144,26 @@ async def wan_recv():
                                                                                        DATA_OFFSET_MOVEMENT)
                 if not chunk_data:
                     raise ValueError("no chunk data")
+                if fragment_part == 31 and not last_fragment:
+                    raise ValueError("last possible fragment part but not last fragment")
             except Exception as e:
                 print("recv-error", e)
                 continue
 
-            if fragment_part == 31 and not last_fragment:
-                ...  # do other things?
-            else:
-                data = await d_handler.new_data_event(data_offset, fragment_part, last_fragment, chunk_data)
-                if data:
-                    try:
-                        data = b32decode_nopad(data)
-                        final_data = data[:-4]
-                        chksum = data[-4:]
-                        assert final_data and len(chksum) == 4 and get_crc32_bytes(final_data, chksum_pass) == chksum
-                    except Exception as e:
-                        print("data-error", e)
+            data = await d_handler.new_data_event(data_offset, fragment_part, last_fragment, chunk_data)
+            if data:
+                try:
+                    data = b32decode_nopad(data)
+                    final_data = data[:-4]
+                    chksum = data[-4:]
+                    assert final_data and len(chksum) == 4 and get_crc32_bytes(final_data, chksum_pass) == chksum
+                except Exception as e:
+                    print("data-error", e)
+                else:
+                    if h_addr_is_fixed:
+                        await loop.sock_sendall(h_inbound_socket, final_data)
                     else:
-                        if h_addr_is_fixed:
-                            await loop.sock_sendall(h_inbound_socket, final_data)
-                        else:
-                            await loop.sock_sendto(h_inbound_socket, final_data, last_h_addr)
+                        await loop.sock_sendto(h_inbound_socket, final_data, last_h_addr)
 
             response = create_noerror_empty_response(qid, qflags, raw_data[12:next_question])
             await loop.sock_sendto(receive_socket, response, addr_w)
